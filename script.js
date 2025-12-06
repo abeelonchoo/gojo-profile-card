2a468f51f819f03f3d95745a73f71843a3aa608a
abeelonchoo <kfcbkjaja@gmail.com>
2025-12-06 19:05:04 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
+const audio = new Audio('audio.mp3');
+audio.loop = true;
+audio.volume = 0.5;
 
-        const card = document.getElementById('card');
+
+audio.play().catch(() => {
+    
+    document.addEventListener('click', () => {
+        audio.play();
+    }, { once: true });
+});
+
+document.getElementById('play-overlay').addEventListener('click', function() {
+    this.style.display = 'none';
+});
+      
+      
+      
+      
+      const card = document.getElementById('card');
         const bunny = document.getElementById('bunny');
 
         card.addEventListener('mousemove', (e) => {
             const rect = card.getBoundingClientRect();
-            const x = e.clientX - rect.left; // posición X dentro del elemento
-            const y = e.clientY - rect.top;  // posición Y dentro del elemento
+            const x = e.clientX - rect.left;
+            const y = e.clientY - rect.top; 
             
             const centerX = rect.width / 2;
             const centerY = rect.height / 2;
             
-            // Calcular rotación (entre -15 y 15 grados)
+          
             const rotateX = ((y - centerY) / centerY) * -5;
             const rotateY = ((x - centerX) / centerX) * 5;
             
@@ -23,13 +41,13 @@
 
             bunny.addEventListener('mousemove', (e) => {
             const rect = bunny.getBoundingClientRect();
-            const x = e.clientX - rect.left; // posición X dentro del elemento
-            const y = e.clientY - rect.top;  // posición Y dentro del elemento
+            const x = e.clientX - rect.left; 
+            const y = e.clientY - rect.top;  
             
             const centerX = rect.width / 2;
             const centerY = rect.height / 2;
             
-            // Calcular rotación (entre -15 y 15 grados)
+     
             const rotateX = ((y - centerY) / centerY) * -5;
             const rotateY = ((x - centerX) / centerX) * 5;
             
